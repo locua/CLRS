af6b8a38db2a2d349b55c9656ac37d8951407c44
Louis <poes997@gmail.com>
2021-01-30 11:36:06 +0000

    merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -1,41 +1,60 @@
 #include <math.h>
 #include <stdio.h>
 
-void MERGE(int arr[], int p, int q, int r)
-{
-    int n1 = q - p + 1;
-    int n2 = r - q;    
+void MERGE(int arr[], int low, int mid, int high){
+    int n1 = mid - low + 1;
+    int n2 = high - mid;    
     
-    float L[n1], R[n2];
+    float L[n1+1], R[n2+1];
 
-    for(int i = 0; i < n1; i++)
-    {
-        L[i] = arr[p + i - 1];
+    for(int i = 0; i < n1; i++){
+        L[i] = arr[low + i];
     }
-    for(int j = 0; j < n1; j++)
-    {
-        R[j] = arr[q + j];
+    for(int j = 0; j < n2; j++){
+        R[j] = arr[mid + j+1];
     }
 
-    L[n1+1] = INFINITY;
-    R[n2+1] = INFINITY;
-    int i, j = 1;
+    L[n1] = INFINITY;
+    R[n2] = INFINITY;
 
-    for(int k = p; k < r; k++)
-    {
-        if(L[i] <= R[j])
-        {
+    int i = 0;
+    int j = 0;
+
+    for(int k = low; k <= high; k++){
+        if(L[i] <= R[j]){
             arr[k] = L[i];
+            i = i + 1;
         }
-        else 
-        {
+        else {
             arr[k] = R[j];
+            j = j + 1;
         }
     }
 } 
 
+
+void MERGE_SORT(int arr[], int low, int high){
+    if(low < high){
+        int mid = floor((low+high)/2);
+        MERGE_SORT(arr, low, mid);
+        MERGE_SORT(arr, mid+1, high);
+        MERGE(arr, low, mid, high);
+    } 
+}
+
+void printArr(int arr[], int size){
+    int i = 0;
+    for(i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+} 
+
 int main()
 {
-
+    int arr[5] = {4, 3, 1, 8, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    //printArr(arr, arr_size);
+    MERGE_SORT(arr, 0, arr_size-1);
+    printArr(arr, arr_size);
     return 0;
 }      
