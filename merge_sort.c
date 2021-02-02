41f6b09d8281ce2163c963853c876fede511c9e7
Louis <poes997@gmail.com>
2021-02-02 00:22:26 +0000

    merge sort without sentinel, less lines

--- merge_sort.c
+++ merge_sort.c
@@ -11,9 +11,7 @@ void printArr(int arr[], int size){
 void MERGE(int arr[], int low, int mid, int high){
     int n1 = mid - low + 1;
     int n2 = high - mid;    
-    
-    //float L[n1+1], R[n2+1]; // for sentinals
-    float L[n1], R[n2]; // 
+    float L[n1], R[n2];
 
     for(int i = 0; i < n1; i++){
         L[i] = arr[low + i];
@@ -22,14 +20,19 @@ void MERGE(int arr[], int low, int mid, int high){
         R[j] = arr[mid + j+1];
     }
 
-    /* Sentinals */
-    //L[n1] = INFINITY; 
-    //R[n2] = INFINITY;
-
     int i =0, j = 0, k = low;
     while(k <= high){
-        if(i >= n1 || j >= n2){
-            break; 
+        if(i>=n1){
+            arr[k]=R[j]; 
+            j++;
+            k++;
+            continue;
+        } 
+        if(j>=n2){
+            arr[k]=L[i]; 
+            i++;
+            k++;
+            continue;
         }
         if(L[i] <= R[j]){
             arr[k] = L[i];
@@ -40,28 +43,13 @@ void MERGE(int arr[], int low, int mid, int high){
         }
         k++;
     }
-    if(i>=n1){
-        while(k<=high){
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-    if(j>=n2){
-        while(k<=high){
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-    }
-    
 } 
 
 
 void MERGE_SORT(int arr[], int low, int high){
     //int arr_size = sizeof(arr) / sizeof(arr[0]);
-    //arr_size=13;
-    //printArr(arr, arr_size);
+    int arr_size=14;
+    printArr(arr, arr_size);
     if(low < high){
         int mid = floor((low+high)/2);
         MERGE_SORT(arr, low, mid);
@@ -73,7 +61,7 @@ void MERGE_SORT(int arr[], int low, int high){
 
 int main()
 {
-    int arr[14] = {69,31,1,18,700,47,25,6,8,73,6,5,67,7};
+    int arr[14] = {69,31,1,18,700,47,69,6,8,73,6,5,67,7};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
     printArr(arr, arr_size);
     MERGE_SORT(arr, 0, arr_size-1);
