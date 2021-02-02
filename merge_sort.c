20de56933c35ea436607a7fe6b1a204551f922de
Louis <poes997@gmail.com>
2021-02-02 00:40:52 +0000

    all in one function mergesort

--- merge_sort.c
+++ merge_sort.c
@@ -8,44 +8,6 @@ void printArr(int arr[], int size){
     printf("\n");
 }
 
-void MERGE(int arr[], int low, int mid, int high){
-    int n1 = mid - low + 1;
-    int n2 = high - mid;    
-    float L[n1], R[n2];
-
-    for(int i = 0; i < n1; i++){
-        L[i] = arr[low + i];
-    }
-    for(int j = 0; j < n2; j++){
-        R[j] = arr[mid + j+1];
-    }
-
-    int i =0, j = 0, k = low;
-    while(k <= high){
-        if(i>=n1){
-            arr[k]=R[j]; 
-            j++;
-            k++;
-            continue;
-        } 
-        if(j>=n2){
-            arr[k]=L[i]; 
-            i++;
-            k++;
-            continue;
-        }
-        if(L[i] <= R[j]){
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-} 
-
-
 void MERGE_SORT(int arr[], int low, int high){
     //int arr_size = sizeof(arr) / sizeof(arr[0]);
     int arr_size=14;
@@ -54,7 +16,41 @@ void MERGE_SORT(int arr[], int low, int high){
         int mid = floor((low+high)/2);
         MERGE_SORT(arr, low, mid);
         MERGE_SORT(arr, mid+1, high);
-        MERGE(arr, low, mid, high);
+        /* MERGE PROCEDURE */
+        int n1 = mid - low + 1;
+        int n2 = high - mid;    
+        float L[n1], R[n2];
+
+        for(int i = 0; i < n1; i++){
+            L[i] = arr[low + i];
+        }
+        for(int j = 0; j < n2; j++){
+            R[j] = arr[mid + j+1];
+        }
+
+        int i =0, j = 0, k = low;
+        while(k <= high){
+            if(i>=n1){
+                arr[k]=R[j]; 
+                j++;
+                k++;
+                continue;
+            } 
+            if(j>=n2){
+                arr[k]=L[i]; 
+                i++;
+                k++;
+                continue;
+            }
+            if(L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
     } 
 }
 
